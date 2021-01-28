007f9524cadc101864606fb3e4746e6d50a70447
wylie <wyliedavid1984@gmail.com>
2021-01-28 17:44:00 -0500

    moved work initailizer from js to html file

--- public/assets/js/serviceWorker.js
+++
@@ -1,8 +0,0 @@
- if ("serviceWorker" in navigator) {
-     window.addEventListener("load", () => {
-         navigator.serviceWorker.register("service-worker.js")
-             .then(reg => {
-                 console.log("We found your service worker file!", reg);
-             });
-     });
- }
