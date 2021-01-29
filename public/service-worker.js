3ec1591396ada8c6b438112b99a79db768af1e87
wylie <wyliedavid1984@gmail.com>
2021-01-28 20:38:55 -0500

    added in reference to db

--- public/service-worker.js
+++ public/service-worker.js
@@ -8,7 +8,7 @@ const FILES_TO_CACHE = [
     "/assets/img/icons/icon-192x192.png",
     "/assets/img/icons/icon-512x512.png",
 ];
-
+console.log("No error yet line 11")
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
--- server.js
+++ server.js
@@ -2,29 +2,26 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
-
+// port for server to run on 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
-
+//middleware
+app.use(express.static("public"));
 app.use(logger("dev"));
-
-app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-app.use(express.static("public"));
-
+app.use(compression());
+//Data base connection
 mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/budget", {
     useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
       useFindAndModify: false
 });
-
 // routes
 app.use(require("./routes/api.js"));
-
+//listening for server port
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
